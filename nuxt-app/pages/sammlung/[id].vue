c7d8c199abae610e948105949396363070c193ea
TabeaMv <tabea.merlevede@gmail.com>
2024-06-02 21:57:54 +0200

    fetched data from fake store API and built product site. However, site has an unfixed error and cards are not shown as wanted

--- nuxt-app/pages/sammlung/[id].vue
+++ nuxt-app/pages/sammlung/[id].vue
@@ -1,14 +1,20 @@
 <template>
     <div>
-        <p>Sammlung von {{ id }}</p>
+        <p>{{ player.title }}</p>
+        <p>{{ player.price }}</p>
+        <p>{{ player.id }}</p>
     </div>
 </template>
 
 <script setup>
     const { id } = useRoute().params
+    const uri = 'https://fakestoreapi.com/products/' + id
+
+    //fetch the product
+    const { data: player } = await useFetch(uri)
 
     definePageMeta({
-        layout: 'sammlung'
+        layout: 'sammlung-layout'
     })
 </script>
 
